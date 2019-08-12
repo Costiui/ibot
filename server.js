7b13bd24d0810c78baef28def7df2c25daa85745
Glitch (hello-express) <none>
2019-08-12 23:17:12 +0000

    🎹🎇 Checkpoint ./server.js:93264/1106

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-// server.js
-// where your node app starts
 
-// init project
-const express = require('express');
-const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
+// Pingable page
+var express = require('express');
+var app = express();
+app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// Keepy uppy
+const http = require('http');
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
