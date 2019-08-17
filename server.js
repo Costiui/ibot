d4249ab6a36c3252c179b6d4790918f50d7d10e1
Glitch (ipfs-stackbot) <none>
2019-08-17 16:47:16 +0000

    🌟🌋 Checkpoint ./server.js:93264/451 ./views/index.html:93264/1856

--- server.js
+++ server.js
@@ -77,3 +77,19 @@ Object.keys(botConfig).forEach(key => {
 });
 
 feedToIRC(botConfig);
+
+// Pingable page
+var express = require('express');
+var app = express();
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// Keepy uppy
+const http = require('http');
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
